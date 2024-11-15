4d04ac60a8bcc66d834407ba40192551ac6b8d64
Bhabishworgrg <bhabishworgrg@gmail.com>
2024-11-15 12:38:14 +0545

    Remove redundant functions

--- script.js
+++ script.js
@@ -8,73 +8,42 @@ const kick_audio = document.getElementById('kick-audio');
 const hihat_audio = document.getElementById('hihat-audio');
 const open_hihat_audio = document.getElementById('open-hihat-audio');
 
+function play_sound(audio) {
+	const clone = audio.cloneNode();
+	document.body.appendChild(clone);
+	clone.play();
+	clone.addEventListener('ended', () => clone.remove());
+}
+
 snare_btn.addEventListener('click', () => {
-	const snare_clone = snare_audio.cloneNode();
-    document.body.appendChild(snare_clone);
-    snare_clone.play();
-    snare_clone.addEventListener('ended', () => {
-        snare_clone.remove();
-	});
+	play_sound(snare_audio);
 });
 
 kick_btn.addEventListener('click', () => {
-	const kick_clone = kick_audio.cloneNode();
-    document.body.appendChild(kick_clone);
-    kick_clone.play();
-    kick_clone.addEventListener('ended', () => {
-        kick_clone.remove();
-	});
+	play_sound(kick_audio);
 });
+
 hihat_btn.addEventListener('click', () => {
-	const hihat_clone = hihat_audio.cloneNode();
-	document.body.appendChild(hihat_clone);
-	hihat_clone.play();
-	hihat_clone.addEventListener('ended', () => {
-		hihat_clone.remove();
-	});
+	play_sound(hihat_audio);
 });
+
 open_hihat_btn.addEventListener('click', () => {
-	const open_hihat_clone = open_hihat_audio.cloneNode();
-	document.body.appendChild(open_hihat_clone);
-	open_hihat_clone.play();
-	open_hihat_clone.addEventListener('ended', () => {
-		open_hihat_clone.remove();
-	});
+	play_sound(open_hihat_audio);
 });
 
 document.addEventListener('keydown', (e) => {
 	switch (e.key) {
 		case 'a':
-			const snare_clone = snare_audio.cloneNode();
-			document.body.appendChild(snare_clone);
-			snare_clone.play();
-			snare_clone.addEventListener('ended', () => {
-				snare_clone.remove();
-			});
+			play_sound(snare_audio);
 			break;
 		case 's':
-			const kick_clone = kick_audio.cloneNode();
-			document.body.appendChild(kick_clone);
-			kick_clone.play();
-			kick_clone.addEventListener('ended', () => {
-				kick_clone.remove();
-			});
+			play_sound(kick_audio);
 			break;
 		case 'd':
-			const hihat_clone = hihat_audio.cloneNode();
-			document.body.appendChild(hihat_clone);
-			hihat_clone.play();
-			hihat_clone.addEventListener('ended', () => {
-				hihat_clone.remove();
-			});
+			play_sound(hihat_audio);
 			break;
 		case 'f':
-			const open_hihat_clone = open_hihat_audio.cloneNode();
-			document.body.appendChild(open_hihat_clone);
-			open_hihat_clone.play();
-			open_hihat_clone.addEventListener('ended', () => {
-				open_hihat_clone.remove();
-			});
+			play_sound(open_hihat_audio);
 			break;
 		default:
 			break;
